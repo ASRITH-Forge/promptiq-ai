16536553bb0a04914397ac9d9664870fb5f17406
asrith-raju <asrithraju143@gmail.com>
2026-02-26 10:30:00 +0530

    server(feat): create api endpoint for getuser function

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { getUser, loginUser, registerUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
+userRouter.get('/data',getUser)
