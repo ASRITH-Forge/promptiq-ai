1edf822c991d942f80ced6e33788476f0213e262
asrith-raju <asrithraju143@gmail.com>
2026-03-15 12:20:51 +0530

    client(feat): enhance fetchUserChats to retrieve chats from API and create new chat if none exist

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, use, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyChats, dummyUserData } from "../assets/assets";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL
 
@@ -48,8 +49,24 @@ export const AppContextProvider = ({children}) => {
         
 
     const fetchUserChats = async () => {
-        setChats(dummyChats)
-        setSelectedChat(dummyChats[0])
+       try {
+        const {data} = await axios.get('/api/chat/get',{headers:{Authorization:token}})
+        if(data.success){
+            setChats(data.chats)
+            // If the user has no chats, create new one
+            if(data.chats.length === 0){
+                await createNewChat()
+                return fetchUserChats()
+            }else{
+                    setSelectedChat(data.chats[0])
+            }
+        }else{
+            toast.error(data.message)
+        }
+
+       } catch (error) {
+            toast.error(error.message) 
+       }
     }
 
     useEffect(()=>{ 
@@ -73,11 +90,17 @@ export const AppContextProvider = ({children}) => {
 
 
     useEffect(()=>{
-        fetchUser()
-    },[])
+        if(token){
+            fetchUser()
+        }else{
+            setUser(null)
+            setLoadingUser(false)
+        }
+        
+    },[token])
 
     const value = {
-        navigate,user,setUser,chats,setChats,selectedChat,setSelectedChat,theme,setTheme
+        navigate,user,setUser,chats,setChats,selectedChat,setSelectedChat,theme,setTheme,createNewChat,loadingUser,fetchUserChats,token,setToken,axios 
     }
     return(
         <AppContext.Provider value={value}>
