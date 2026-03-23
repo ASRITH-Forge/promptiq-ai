7c45939f099bacd35d80b08cf94cfc3d4df61b15
asrith-raju <asrithraju143@gmail.com>
2026-03-23 18:39:58 +0530

    client(feat): implement purchasePlan function to handle credit plan purchases

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -23,6 +23,19 @@ const Credits = () => {
     setLoading(false) 
   }
 
+  const purchasePlan = async(planId)=>{
+    try {
+      const {data} = await axios.post('/api/credit/purchase',{planId},{headers:{Authorization:`Bearer ${token}`}})
+      if(data.success){
+        window.location.href=data.url
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   useEffect(() => {
     fetchPlans()
   }, [])
@@ -45,7 +58,7 @@ const Credits = () => {
                   ))}
                 </ul>
             </div>
-            <button className='mt-6 bg-purple-600 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer'>Buy Now</button>
+            <button onClick={()=>toast.promise(purchasePlan(plan._id),{loading:'Processing....'})} className='mt-6 bg-purple-600 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer'>Buy Now</button>
           </div>
         ))} 
       </div>
