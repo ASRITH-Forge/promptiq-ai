d572cf53187bf489bc0cc3e766287daf411d7733
asrith-raju <asrithraju143@gmail.com>
2026-03-23 18:09:02 +0530

    client(fix): update image fetching logic and correct username casing in Community component

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { dummyPublishedImages } from '../assets/assets'
 import Loading from './Loading'
+import { useAppContext } from '../context/AppContext'
 
 const Community = () => {
   const [images, setImages] = useState([])
   const [loading,setLoading] = useState(true)
+  const {axios} = useAppContext()
 
   const fetchImages = async()=>{
-    setImages(dummyPublishedImages)
-    setLoading(false)
+   try {
+    const {data} = await axios.get('/api/user/published-images')
+    if(data.success){
+      setImages(data.images)
+    }else{
+      toast.error(data.message)
+    }
+   } catch (error) {
+    toast.error(error.message)
+   }
+   setLoading(false)
   }
   useEffect(() => {
     fetchImages()
@@ -24,7 +35,7 @@ const Community = () => {
           {images.map((item,index)=>(
             <a key={index} href={item.imageUrl} target='_blank' className='relative group block rounded-lg overflow-hidden border border-gray-200 dark:border-purple-700 shadow-sm hover:shadow-md transition-shadow duration-300'>
               <img src={item.imageUrl} alt=""  className='w-full h-40 md:h-50 2xl:h-62 object-cover group-hover:scale-105 transition-transform-duration-300 ease-in-out'/>
-              <p className='absolute bottom-0 right-0 text-xs bg-black/50 backdrop-blur text-white px-4 py-1 rounded-tl-xl opacity-0 group-hover:opacity-100 transition duration-300'>Created by {item.userName}</p>
+              <p className='absolute bottom-0 right-0 text-xs bg-black/50 backdrop-blur text-white px-4 py-1 rounded-tl-xl opacity-0 group-hover:opacity-100 transition duration-300'>Created by {item.username}</p>
             </a>
           ))}
         </div>
